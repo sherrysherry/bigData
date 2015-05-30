97d0fe148216551bc603433e88e5516d1531b0fa
Sherry Chen <sherryyingting@gmail.com>
2015-05-31 00:56:02 +0800

    footer page

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <title>Data Visualization</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/dashboard.css">
+	<link rel="stylesheet" type="text/css" href="style/jquery-ui.css">
+	
+	<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script src="js/velocity.min.js" type="text/javascript"></script>
+	<script src="js/jquery-ui.min.js" type="text/javascript"></script>
+	<script src="js/d3.v3.min.js" type="text/javascript"></script>
+	<script src="js/d3.geo.projection.v0.min.js" type="text/javascript"></script>
+	<script src="js/queue.v1.min.js" type="text/javascript"></script>
+	<script src="js/topojson.v1.min.js" type="text/javascript"></script>
+	<script src="js/init.js" type="text/javascript"></script>
+	<script src="js/visualization.js" type="text/javascript"></script>
+	<script src="js/dashboard.js" type="text/javascript"></script>
+	<script src="js/canvasjs.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="contentContainer">
